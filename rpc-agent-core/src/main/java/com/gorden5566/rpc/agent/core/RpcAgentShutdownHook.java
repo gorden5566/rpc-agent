23e583f891e9f36e63d2fafa5a736249c5bcd192
gorden5566 <gorden5566@126.com>
2020-08-18 01:05:56 +0800

    RpcAgentShutdownHook

---
+++ rpc-agent-core/src/main/java/com/gorden5566/rpc/agent/core/RpcAgentShutdownHook.java
@@ -0,0 +1,47 @@
+package com.gorden5566.rpc.agent.core;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author gorden5566
+ * @date 2020/08/17
+ */
+public class RpcAgentShutdownHook extends Thread {
+    private static final RpcAgentShutdownHook INSTANCE = new RpcAgentShutdownHook("RpcAgentShutdownHook");
+
+    private final AtomicBoolean registered = new AtomicBoolean(false);
+    private final AtomicBoolean destroyed = new AtomicBoolean(false);
+
+    private RpcAgentShutdownHook(String name) {
+        super(name);
+    }
+
+    public static RpcAgentShutdownHook getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void run() {
+        doDestroy();
+    }
+
+    public void register() {
+        if (!registered.get() && registered.compareAndSet(false, true)) {
+            Runtime.getRuntime().addShutdownHook(getInstance());
+        }
+    }
+
+    public void unregister() {
+        if (registered.get() && registered.compareAndSet(true, false)) {
+            Runtime.getRuntime().removeShutdownHook(getInstance());
+        }
+    }
+
+    public void doDestroy() {
+        if (!destroyed.compareAndSet(false, true)) {
+            return;
+        }
+        // doDestroy
+
+    }
+}
