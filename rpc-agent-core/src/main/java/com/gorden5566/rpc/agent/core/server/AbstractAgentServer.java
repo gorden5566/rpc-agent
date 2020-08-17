5942cfd50ea689cde8f7c24f42b2db1c8eaf688d
gorden5566 <gorden5566@126.com>
2020-08-18 01:14:12 +0800

    JettyAgentServer

---
+++ rpc-agent-core/src/main/java/com/gorden5566/rpc/agent/core/server/AbstractAgentServer.java
@@ -0,0 +1,43 @@
+package com.gorden5566.rpc.agent.core.server;
+
+import com.gorden5566.rpc.agent.core.AgentServer;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author gorden5566
+ * @date 2020/08/17
+ */
+public abstract class AbstractAgentServer implements AgentServer {
+    protected static final String INVOKER_PATH = "/agent";
+
+    protected String host;
+    protected int port;
+
+    public AbstractAgentServer(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    protected String formatAddress(InetSocketAddress address, String path) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(address.getHostName()).append(":").append(address.getPort()).append(path);
+        return sb.toString();
+    }
+}
---
+++ rpc-agent-core/src/main/java/com/gorden5566/rpc/agent/core/server/JettyAgentServer.java
@@ -0,0 +1,43 @@
+package com.gorden5566.rpc.agent.core.server;
+
+import com.gorden5566.rpc.agent.core.servlet.InvokerServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author gorden5566
+ * @date 2020/08/18
+ */
+public class JettyAgentServer extends AbstractAgentServer {
+
+    private Server server;
+
+    public JettyAgentServer(String host, int port) {
+        super(host, port);
+    }
+
+    @Override
+    public void start() throws Exception {
+        InetSocketAddress address = new InetSocketAddress(host, port);
+        server = new Server(address);
+
+        ServletContextHandler context = new ServletContextHandler(
+            ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(InvokerServlet.class, INVOKER_PATH);
+
+        server.start();
+        System.out.println("server started, agent path: [" + formatAddress(address, INVOKER_PATH) + "]");
+
+        server.join();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        server.stop();
+    }
+}
