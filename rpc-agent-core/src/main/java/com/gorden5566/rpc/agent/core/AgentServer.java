c8b445e5570f2914264295ad5b5597e90b57b00d
gorden5566 <gorden5566@126.com>
2020-08-17 23:15:03 +0800

    AgentServer

---
+++ rpc-agent-core/src/main/java/com/gorden5566/rpc/agent/core/AgentServer.java
@@ -0,0 +1,22 @@
+package com.gorden5566.rpc.agent.core;
+
+/**
+ * @author gorden5566
+ * @date 2020/08/17
+ */
+public interface AgentServer {
+
+    /**
+     * start
+     *
+     * @throws Exception
+     */
+    void start() throws Exception;
+
+    /**
+     * stop
+     *
+     * @throws Exception
+     */
+    void stop() throws Exception;
+}
