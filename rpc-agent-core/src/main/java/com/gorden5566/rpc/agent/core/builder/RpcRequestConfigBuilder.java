f3b3f308d46ded667aab7d14a3f74dd444bf388b
gorden5566 <gorden5566@126.com>
2020-08-18 01:39:53 +0800

    RpcRequestConfigBuilder

--- rpc-agent-core/src/main/java/com/gorden5566/rpc/agent/core/builder/RpcRequestConfigBuilder.java
+++ rpc-agent-core/src/main/java/com/gorden5566/rpc/agent/core/builder/RpcRequestConfigBuilder.java
@@ -1,11 +1,12 @@
 package com.gorden5566.rpc.agent.core.builder;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.alibaba.fastjson.JSON;
 import com.gorden5566.rpc.agent.core.model.RpcRequestConfig;
 import com.gorden5566.rpc.agent.core.util.HttpUtils;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 /**
  * @author gorden5566
@@ -24,7 +25,7 @@ public class RpcRequestConfigBuilder {
     }
 
     public RpcRequestConfig build() throws IOException {
-            String bodyString = HttpUtils.getBodyString(request);
-            return JSON.parseObject(bodyString, RpcRequestConfig.class);
+        String bodyString = HttpUtils.getBodyString(request);
+        return JSON.parseObject(bodyString, RpcRequestConfig.class);
     }
 }
