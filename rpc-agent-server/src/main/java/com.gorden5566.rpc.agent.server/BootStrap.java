db7f66236f99af2f6f9d2fc777b4863625a86beb
gorden5566 <gorden5566@126.com>
2020-08-20 02:08:16 +0800

    SPECIFICATION_TITLE

--- rpc-agent-server/src/main/java/com.gorden5566.rpc.agent.server/BootStrap.java
+++ rpc-agent-server/src/main/java/com.gorden5566.rpc.agent.server/BootStrap.java
@@ -2,18 +2,27 @@ package com.gorden5566.rpc.agent.server;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * @author gorden5566
  * @date 2020/08/19
  */
 public class BootStrap {
+    private static String SPECIFICATION_TITLE = "rpc-agent-server";
+    static {
+        String specificationTitle = BootStrap.class.getPackage().getSpecificationTitle();
+        if (StringUtils.isNotBlank(specificationTitle)) {
+            SPECIFICATION_TITLE = specificationTitle;
+        }
+    }
+
     public static void main(String[] args) {
         Command command = new Command();
 
         JCommander jc = JCommander.newBuilder()
             .addObject(command)
-            .programName("java -jar rpc-agent-server.jar")
+            .programName("java -jar " + SPECIFICATION_TITLE + ".jar")
             .build();
 
         try {
