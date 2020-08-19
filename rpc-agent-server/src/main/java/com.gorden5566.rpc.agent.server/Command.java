40522be5a1c5c3beb56747823bb37129ea58097e
gorden5566 <gorden5566@126.com>
2020-08-20 01:25:08 +0800

    support console in server mode

--- rpc-agent-server/src/main/java/com.gorden5566.rpc.agent.server/Command.java
+++ rpc-agent-server/src/main/java/com.gorden5566.rpc.agent.server/Command.java
@@ -1,20 +1,27 @@
 package com.gorden5566.rpc.agent.server;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.gorden5566.rpc.agent.core.internal.InstanceFactory;
 import com.gorden5566.rpc.agent.core.internal.RpcRequestConfig;
+import com.gorden5566.rpc.agent.core.spi.InvokerProxy;
 import com.gorden5566.rpc.agent.core.util.JsonUtils;
 import com.gorden5566.rpc.agent.server.util.NetUtils;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * @author gorden5566
  * @date 2020/08/19
  */
 public class Command {
+    private static final String DEFAULT_HOST = "127.0.0.1";
 
     @Parameter(names = {"-h", "--help"}, order = 1, description = "print help message")
     private boolean help = false;
@@ -22,10 +29,16 @@ public class Command {
     @Parameter(names = {"-d", "--demo"}, order = 2, description = "export config demo")
     private boolean demo = false;
 
-    @Parameter(names = {"-l", "--local"}, order = 4, description = "[server mode] use loopback address")
+    @Parameter(names = {"-f", "--file"}, order = 3, description = "[client mode] specified config file")
+    private String file = "request.json";
+
+    @Parameter(names = {"-s", "--server"}, order = 4, description = "[server mode] enable server mode")
+    private boolean server = false;
+
+    @Parameter(names = {"-l", "--local"}, order = 5, description = "[server mode] use loopback address")
     private boolean local = false;
 
-    @Parameter(names = {"-p", "--port"}, order = 5, description = "[server mode] specified server port")
+    @Parameter(names = {"-p", "--port"}, order = 6, description = "[server mode] specified server port")
     private int port = 8080;
 
     public void run(JCommander jc) {
@@ -34,18 +47,31 @@ public class Command {
             return;
         }
 
+        if (server) {
+            String host = local ? DEFAULT_HOST : NetUtils.getFirstLocalIp();
+            AgentServer server = new JettyAgentServer(host, port);
+            try {
+                server.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+
         if (demo) {
             export("demo.json");
             return;
         }
 
-        String host = local ? "127.0.0.1" : NetUtils.getFirstLocalIp();
-        AgentServer server = new JettyAgentServer(host, port);
-        try {
-            server.start();
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (StringUtils.isNotBlank(file)) {
+            boolean success = process(file);
+            if (!success) {
+                jc.usage();
+            }
+            return;
         }
+
+        jc.usage();
     }
 
     private void export(String fileName) {
@@ -66,5 +92,33 @@ public class Command {
         } catch (IOException e) {
             System.out.println("write config file failed: " + e.getMessage());
         }
+    }
+
+    private boolean process(String fileName) {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            System.out.println("config file not exist [" + fileName + "]");
+            return false;
+        }
+
+        RpcRequestConfig config = null;
+        try {
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            config = JSON.parseObject(bytes, RpcRequestConfig.class);
+        } catch (Exception e) {
+            System.out.println("read config file failed: " + e.getMessage());
+        }
+
+        try {
+            String result = getInvokerProxy().invoke(config);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("remote call failed: " + e.getMessage());
+        }
+        return true;
+    }
+
+    private InvokerProxy getInvokerProxy() {
+        return InstanceFactory.getInvokerProxy();
     }
 }
