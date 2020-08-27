2af226b7710883295b899ae38434f0e942fd62e1
gorden5566 <gorden5566@126.com>
2020-08-28 00:30:42 +0800

    config equals

--- rpc-agent-core/src/main/java/com/gorden5566/rpc/agent/core/internal/config/InvokerConfig.java
+++ rpc-agent-core/src/main/java/com/gorden5566/rpc/agent/core/internal/config/InvokerConfig.java
@@ -2,6 +2,8 @@ package com.gorden5566.rpc.agent.core.internal.config;
 
 import com.gorden5566.rpc.agent.core.internal.RpcRequestConfig;
 
+import java.util.Objects;
+
 /**
  * @author gorden5566
  * @date 2020/08/25
@@ -32,5 +34,23 @@ public class InvokerConfig {
 
     public void setPort(Integer port) {
         this.port = port;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        InvokerConfig config = (InvokerConfig) o;
+        return Objects.equals(host, config.host) &&
+            Objects.equals(port, config.port);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(host, port);
     }
 }
